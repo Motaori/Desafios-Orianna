e0ccf4b7c7b87b5553c4b43d1619feea5c191fec
Orianna <oriannamota07@gmail.com>
2024-04-19 22:16:45 -0300

    desafio 1

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let prod="celular"
+let precio=9000
+let cantidad
+
+alert("hola estas por comprar un "+prod+" que cuesta "+precio)
+let cantidaddeseada = prompt("le pido que ingrese la cantidad que desea comprar: ")
+
+
+const preciototal = precio*cantidaddeseada
+
+alert("usted compro "+ cantidaddeseada+" "+ prod+".El costo total es de "+preciototal+" dolares")
